b8c62515904e3c8b9a09398efaaafd330c2de89d
ZeevKeane <63415815+ZeevKeane@users.noreply.github.com>
2021-12-08 08:10:16 -0500

    Add files via upload

---
+++ countdown.js
@@ -0,0 +1,30 @@
+(function () {
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+
+    launchdate = "12/14/2021"
+
+    const countDown = new Date(launchdate).getTime(),
+        x = setInterval(function () {
+
+            const now = new Date().getTime(),
+                distance = countDown - now;
+
+            document.getElementById("days").innerText = Math.floor(distance / (day)),
+                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+            //do something later when date is reached
+            if (distance < 0) {
+                document.getElementById("headline").innerText = "THE BAKERY IS ONLINE";
+                document.getElementById("countdown").style.display = "none";
+                document.getElementById("content").style.display = "block";
+                clearInterval(x);
+            }
+            //seconds
+        }, 0)
+}());
